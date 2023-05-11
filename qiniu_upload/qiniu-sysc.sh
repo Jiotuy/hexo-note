5fc4d65d689b2c05ea11a197146c7fda613c3d09
tianxiawushuang90 <tianxiawushuang90@outlook.com>
2023-05-11 15:00:05 +0800

    test

--- qiniu_upload/qiniu-sysc.sh
+++ qiniu_upload/qiniu-sysc.sh
@@ -3,4 +3,4 @@ wget https://devtools.qiniu.com/qshell-v2.10.0-linux-386.tar.gz
 tar xzvf qshell-v2.10.0-linux-386.tar.gz
 
 ./qshell account $QINIU_ACCESS_KEY $QINIU_SECRET_KEY $QINIU_USER_NAME
-./qshell qupload2 --src-dir=$HEXO_PUBLIC_FILE --bucket=$QINIU_BUCKET --overwrite=true --rescan-local=true --check_exists=true --check_hash=true
+./qshell qupload2 --src-dir=$HEXO_PUBLIC_FILE --bucket=$QINIU_BUCKET --check-exists=true --check-hash=true --overwrite=true --rescan-local=true 
